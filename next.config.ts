27b3815efdb988e869571b203e2a011e3e58d293
thekingofcool <sayhi@thekingof.cool>
2025-08-01 16:17:02 +0800

    update

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
+  output: 'export',
   images: {
     unoptimized: true,
   },
   typescript: {
     ignoreBuildErrors: false,
   },
+  compress: true,
 };
 
 export default nextConfig;
